53191c4c8f5921d1685c390715869f6695e1c359
JIN HOE LIM <jinlim@swin.edu.au>
2021-08-15 16:35:56 +1000

    Hide cursor on tablet and mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AnimatedCursor from "react-animated-cursor";
 
 import "./styles/App.css";
+import constants from "./utils/constants";
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
@@ -13,19 +14,83 @@ import BackToTop from "./components/BackToTop";
 import Time from "./components/Time";
 import NotFound from "./pages/NotFound";
 
-class App extends React.Component {
+interface IState {
+  isDesktop: boolean;
+  isTablet: boolean;
+}
+
+class App extends React.Component<any, IState> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDesktop: false,
+      isTablet: false,
+    };
+  }
+
+  componentDidMount() {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    this.setState({
+      isDesktop: window.matchMedia(
+        "(min-width: " + constants.desktopWidth + "px)"
+      ).matches,
+      isTablet: window.matchMedia(
+        "(min-width: " +
+          constants.tabletWidth +
+          "px) and (max-width: " +
+          (constants.desktopWidth - 1) +
+          "px)"
+      ).matches,
+    });
+
+    window.addEventListener("resize", () => {
+      this.setState({
+        isDesktop: window.matchMedia(
+          "(min-width: " + constants.desktopWidth + "px)"
+        ).matches,
+        isTablet: window.matchMedia(
+          "(min-width: " +
+            constants.tabletWidth +
+            "px) and (max-width: " +
+            (constants.desktopWidth - 1) +
+            "px)"
+        ).matches,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", () => {
+      this.setState({
+        isDesktop: window.matchMedia(
+          "(min-width: " + constants.desktopWidth + "px)"
+        ).matches,
+        isTablet: window.matchMedia(
+          "(min-width: " +
+            constants.tabletWidth +
+            "px) and (max-width: " +
+            (constants.desktopWidth - 1) +
+            "px)"
+        ).matches,
+      });
+    });
+  }
+
   render() {
     return (
       <Fragment>
         <Router>
-          <AnimatedCursor
-            innerSize={"0.5vw"}
-            outerSize={"1.5vw"}
-            color="0, 0, 0"
-            outerAlpha={0.2}
-            outerScale={1.5}
-            trailingSpeed={6}
-          />
+          {this.state.isDesktop && (
+            <AnimatedCursor
+              innerSize={"0.5vw"}
+              outerSize={"1.5vw"}
+              color="0, 0, 0"
+              outerAlpha={0.2}
+              outerScale={1.5}
+              trailingSpeed={6}
+            />
+          )}
           <BackToTop />
           <Time />
           <Navbar />
